da14f82d48f4fc8797d33720b12db812a0d1e5f7
EinLeith <86112563+EinLeith@users.noreply.github.com>
2021-09-13 11:18:34 +0200

    Add files via upload

---
+++ rechner.py
@@ -0,0 +1,10 @@
+while True 
+    Zahl1 = input("Gib Die erste Zahl ein: ")
+    Zahl2 = input("Gib die Zweite Zahl ein: ")
+
+    Zahl1 = int(Zahl1)
+    Zahl2 = int(Zahl2)
+
+    Ergebnis = Zahl1 * Zahl2
+
+print(Ergebnis) 
